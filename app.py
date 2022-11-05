9e94ffc53fdcaa316c9b45c85d6ecdfc7f80adde
manav <mmalhotra_be21@thapar.edu>
2022-11-05 17:34:02 +0530

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+# importing host library
+import streamlit as manav
+
+# summary package for project
+
+# web scrapping packages for the project 
+
+
+
+
+def  main():
+    # summary and entity checker
+
+    manav.title("Summarizer and Entity Checker")
+
+
+if __name__ == "__main__":
+    main()
